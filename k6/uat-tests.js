0e8ae77b52f58538815b1307c0ee045e6643f362
fuentesivaroEveris <86650181+fuentesivaroEveris@users.noreply.github.com>
2021-11-19 10:33:43 +0100

    Update uat-tests.js

--- k6/uat-tests.js
+++ k6/uat-tests.js
@@ -1,5 +1,18 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 15 },
+    { duration: '20s', target: 15 },
+    { duration: '10s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<250'],
+  },
+};
 
 export default function () {
-  http.get('http://test.k6.io');
+  const res = http.get('https://sandbox.apis.who.int:443/who/dev/example-cicd-validation/api/v1/validation');
+  sleep(1);
 }
